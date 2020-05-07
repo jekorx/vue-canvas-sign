b61ef97b2e43463d773668f8df35d395ded93718
wang_dgang <wdg404@163.com>
2020-05-07 18:02:38 +0800

    修复无法import的问题

--- src/index.js
+++ src/index.js
@@ -1,19 +1,3 @@
 import CanvasSign from './canvas-sign'
 
-const install = function (Vue) {
-  Vue.component(CanvasSign.name, CanvasSign)
-}
-
-/* istanbul ignore if */
-if (typeof window !== 'undefined' && window.Vue) {
-  install(window.Vue)
-}
-
-export default {
-  install,
-  CanvasSign
-}
-
-export {
-  CanvasSign
-}
+export default CanvasSign
